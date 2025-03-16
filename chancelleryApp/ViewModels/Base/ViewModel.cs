3e98d9b9a42867db79ecb3ac1ee8d79cb9a4e7ca
Work <Work@TAZYPC>
2025-03-16 20:41:10 +0900

    Добавьте файлы проекта.

---
+++ chancelleryApp/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chancelleryApp.ViewModels.Base
+{
+    public abstract class ViewModel: INotifyPropertyChanged
+    {
+
+    }
+}
